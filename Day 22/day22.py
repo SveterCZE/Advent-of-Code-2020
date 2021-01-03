4941fbcf4a24f83d0bee06de47a3f4b1d844cd2e
Petr Šmerkl <46304018+SveterCZE@users.noreply.github.com>
2021-01-03 20:21:37 +0100

    Update day22.py

--- Day 22/day22.py
+++ Day 22/day22.py
@@ -1,6 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  3 00:55:09 2021
+
+@author: petrs
+"""
+
 def main():
     p1, p2 = get_initial_input()
-    part1(p1, p2)
+    part1(p1.copy(), p2.copy())
+    part2(p1.copy(), p2.copy())
+    return
     
 def get_initial_input():
     f = open("input.txt", "r")
@@ -46,6 +55,43 @@ def counter(winning_hand):
 def part1(p1, p2):
     winning_hand = game(p1, p2)
     print(counter(winning_hand))
-    
 
+def part2(p1, p2):
+    winning_player, winning_hand = recursive_game(p1,p2)
+    print(counter(winning_hand))    
+
+def recursive_game(p1, p2):
+    memo = []
+    while True:
+        if p1 in memo:
+            return 1, p1
+        else:
+            memo.append(p1.copy())
+        played = [p1[0], p2[0]]
+        p1.pop(0)
+        p2.pop(0)
+        # Check if recursive combat occurs
+        if played[0] <= len(p1) and played[1] <= len(p2):
+            winner, his_hand = recursive_game(p1.copy()[:played[0]], p2.copy()[:played[1]])
+            if winner == 1:
+                    p1.append(played[0])
+                    p1.append(played[1])
+            else:
+                    p2.append(played[1])
+                    p2.append(played[0])         
+        # Regular-non-recursive combat
+        else:
+            if played[0] > played[1]:
+                played.sort(reverse = True)
+                for elem in played:
+                    p1.append(elem)
+            else:
+                played.sort(reverse = True)
+                for elem in played:
+                    p2.append(elem)
+        if len(p1) == 0 or len(p2) == 0:
+            if len(p1) == 0:
+                return 2, p2
+            else:
+                return 1, p1
 main()
